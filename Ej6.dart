a802d5e467478d4eef2a05008a478add3a2c6b7b
El Diego <SirDaXll@users.noreply.github.com>
2023-05-20 22:22:27 -0400

    Create Ej6.dart

---
+++ Ej6.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+List<int> multiplicar(List<int> lista1, List<int> lista2) {
+  // assert(lista1.length == lista2.length);    [comprobación largo de listas para poder multiplicar]
+  List<int> resultado = [];
+  for (int i = 0; i < lista1.length; i++) {   // lista1.length = 5 para este caso
+    resultado.add(lista1[i] * lista2[i]);
+  }
+  return resultado;
+}
+
+// Generar lista según el largo indicado y que posea elementos comprendidos entre los numeros dados
+List<int> generarNegativos(int length, int min, int max) {
+  List<int> lista = [];
+  for (int i = 0; i < length; i++) {
+    int numeroAleatorio = min + Random().nextInt(max - min + 1);
+    lista.add(numeroAleatorio);
+  }
+  return lista;
+}
+
+void main() {
+  // Definir las dos listas a multiplicar
+  List<int> lista1 = [4, 3, 2, 2, 1];
+  List<int> lista2 = [-3, 2, 8, 0, 1];
+
+  // Multiplicar las dos listas
+  List<int> resultado = multiplicar(lista1, lista2);
+  print('Resultado de la multiplicación:\n$resultado');
+
+  // Generar la lista aleatoria de números negativos
+  List<int> listaAleatoria = generarNegativos(5, -5, -1);
+  print('Lista aleatoria de números negativos:\n$listaAleatoria');
+
+  // Concatenar el resultado de la multiplicación con la lista aleatoria
+  List<int> listaConcatenada = [...resultado, ...listaAleatoria];
+  print('Lista concatenada:\n$listaConcatenada');
+
+  // Ordenar la lista de forma descendente
+  listaConcatenada.sort((a, b) => b.compareTo(a));
+  print('Lista ordenada de forma descendente:\n$listaConcatenada');
+}
